6f78c5746f99c646ce332b75c19d27bbd92d418d
Shane Stephenson <shanestephenson@Shanes-MacBook-Pro.local>
2021-01-24 23:10:35 -0500

    styling links

--- src/App.js
+++ src/App.js
@@ -5,25 +5,41 @@ import Button from '@material-ui/core/Button';
 import './App.css';
 import About from './containers/About/About';
 import Projects from './containers/Projects/Projects';
+import Landing from './containers/Landing/Landing';
+import Contact from './containers/Contact/Contact';
 
-const app = props => (
+const app = props => {
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'black',
+    fontSize: '22px',
+    margin: '10px'
+  };
+
+  const activeLinkStyle = {
+    textDecoration: 'underline',
+    fontSize: '24px'
+  }
+
+  return (
   <BrowserRouter>
     <div className="App">
-      {/*<Button variant="contained" color="primary">Click Me</Button> */}  
-
-      <ul>
-        <li><NavLink to="/" activeStyle={{ color: 'red' }} >Shane Stephenson</NavLink></li>
-        <li><NavLink to="/about" activeStyle={{ color: 'red' }}>About</NavLink></li>
-        <li><NavLink to="/projects" activeStyle={{ color: 'red' }}>Projects</NavLink></li>
-      </ul>
+      {/*<Button variant="contained" color="primary">Click Me</Button> */}       
+        <NavLink to="/" exact style={linkStyle} activeStyle={activeLinkStyle}>Shane Stephenson</NavLink>
+        <NavLink to="/projects" style={linkStyle} activeStyle={activeLinkStyle}>Projects</NavLink>
+        <NavLink to="/about" style={linkStyle} activeStyle={activeLinkStyle}>About</NavLink>
+        <NavLink to="/contact" style={linkStyle} activeStyle={activeLinkStyle}>Contact</NavLink>
+      
       <Switch>
         <Route exact path="/about" component={About} />
         <Route exact path="/projects" component={Projects} />
-        <Route  path="/"/>
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/" onlyActiveOnIndex component={Landing}/>
       </Switch>
     </div>
   </BrowserRouter>
-);
+  );
+};
 
 
 export default app;
